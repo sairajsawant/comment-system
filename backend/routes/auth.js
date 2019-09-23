028bf4ad7d77c22850481bb0f61950d041c7fb15
Sairaj Sawant <sairajsawant30@gmail.com>
2019-09-23 08:39:18 +0530

    added login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -17,15 +17,22 @@ router.post('/register',async (req, res) => {
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user : savedUser._id });
     }catch(err){
         res.status(400).send(err);
     }
 
 });
 
-// router.post('/login', (req, res) => {
+router.post('/login',async (req, res) => {
+    const user = await User.findOne({username : username});
+    if(! user ) return res.status(400).send('username does not exist, please register!');
 
-// });
+    const validPass = await bcrypt.compare(req.body.password, user.hash);
+    if(!validPass) return res.status(400).send('Password is invalid!');
+
+    res.json({success: true, message: 'Logged in'});
+
+});
 
 module.exports = router
